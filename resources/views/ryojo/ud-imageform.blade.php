614a2f9525623737c30618c7f38ffa32aa6aa925
shingoayane <a113roof@gmail.com>
2023-01-29 21:32:51 +0900

    fix:画像編集挙動修正

--- resources/views/ryojo/ud-imageform.blade.php
+++ resources/views/ryojo/ud-imageform.blade.php
@@ -13,3 +13,60 @@
     <button type="submit" class="block w-1/2 md:w-1/4 h-12 m-auto my-20 bg-[#001a1a] hover:bg-[#1e3333] text-center text-white rounded-lg">次へ</button>
 </x-layoutgray>
 
+<script>
+function setImg(){
+    return new Promise(function(resolve){
+        const resizedImgs = document.querySelectorAll(`[id^='image-']`);
+        const ImgUrls = [];
+        const ImgFiles = [];
+
+        resizedImgs.forEach((resizedImg,i) => {
+            Url = resizedImg.src;
+            ImgUrls.push(Url);
+        });
+
+        ImgUrls.forEach((ImgUrl,i) => {     
+            const bin = atob(ImgUrl.replace(/^.*,/, '')); //baseをバイナリ化           
+            const buffer = new Uint8Array(bin.length);//バイナリデータに変換する
+            for (let i = 0; i < bin.length; i++) {
+                    buffer[i] = bin.charCodeAt(i);
+                }
+            const ImgFile = new File([buffer.buffer],ImgUrl,{type:"image/png"});//Fileオブジェクトを生成
+            ImgFiles.push(ImgFile);
+        });
+    resolve(ImgFiles);
+    });
+}
+
+async function postImg(){
+    const ImgFiles = await setImg();
+
+    const form = document.createElement("form");
+    form.action = "{{ route('ryojo.confirm') }}";
+    form.method = "POST";
+    form.enctype="multipart/form-data";
+
+    const formData = new FormData();
+    ImgFiles.forEach((ImgFile,i) => {
+        formData.append("images[]",ImgFile);
+        });
+
+    form.addEventListener("formdata",e =>{
+        for(const[name,value] of formData.entries()){
+            e.formData.append(name,value)
+        }
+    })
+
+    document.body.append(form);
+
+    const input = document.createElement('input');
+    const csrf = document.querySelector('meta[name="csrf-token"]').content;
+    input.setAttribute('type','hidden');
+    input.setAttribute('name','_token');
+    input.setAttribute('value',csrf);
+
+    form.appendChild(input);
+
+    form.submit(); 
+}
+</script>
